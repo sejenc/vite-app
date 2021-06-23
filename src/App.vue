434d8216be02aaaaea476370e18742edb8843149
Sean Jennings <sejenc@gmail.com>
2021-06-23 09:18:59 -0400

    boilerplate vite set up

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    {{ title }}
+  </div>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        title: 'Politico FE - task'
+      }
+    }
+  }
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
